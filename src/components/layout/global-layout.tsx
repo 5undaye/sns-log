447b20033b979e402c748396696c55c4f0dfa8df
5undaye <5undaye@naver.com>
2026-02-02 22:56:03 +0900

    feat: 프로필 버튼 구현

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -2,7 +2,8 @@ import { Link, Outlet } from "react-router";
 import { SunIcon } from "lucide-react";
 
 import logo from "@/assets/logo.png";
-import defaultAvatar from "@/assets/default-avatar.jpg";
+
+import ProfileButton from "@/components/layout/header/profile-button";
 
 export default function GlobalLayout() {
   return (
@@ -21,7 +22,7 @@ export default function GlobalLayout() {
             <div className="hover:bg-muted cursor-pointer rounded-full p-2">
               <SunIcon />
             </div>
-            <img className="h-6" src={defaultAvatar} />
+            <ProfileButton />
           </div>
         </div>
       </header>
