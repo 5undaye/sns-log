40ba7f52dbe557b1ecb6a7b47da17ab636f6b893
5undaye <5undaye@naver.com>
2026-02-07 23:03:11 +0900

    feat: 프로필 수정 기능 구현

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,3 +1,4 @@
+import { deleteImagesInPath, uploadImage } from "@/api/image";
 import supabase from "@/lib/supabase";
 import { getRandomNickname } from "@/lib/utils";
 
@@ -19,6 +20,44 @@ export async function createProfile(userId: string) {
       id: userId,
       nickname: getRandomNickname(),
     })
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function updateProfile({
+  userId,
+  nickname,
+  bio,
+  avatarImageFile,
+}: {
+  userId: string;
+  nickname?: string;
+  bio?: string;
+  avatarImageFile?: File;
+}) {
+  if (avatarImageFile) {
+    await deleteImagesInPath(`${userId}/avatar`);
+  }
+
+  let newAvatarImageUrl;
+
+  if (avatarImageFile) {
+    const fileExtension = avatarImageFile.name.split(".").pop() || "webp";
+    const filePath = `${userId}/avatar/${new Date().getTime()}-${crypto.randomUUID()}.${fileExtension}`;
+
+    newAvatarImageUrl = await uploadImage({
+      file: avatarImageFile,
+      filePath: filePath,
+    });
+  }
+
+  const { data, error } = await supabase
+    .from("profile")
+    .update({ nickname, bio, avatar_url: newAvatarImageUrl })
+    .eq("id", userId)
     .select()
     .single();
 
