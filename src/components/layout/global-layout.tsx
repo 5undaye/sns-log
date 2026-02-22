98159c440e78e7d1e879b1c02827bb0c4cafd48a
5undaye <5undaye@naver.com>
2026-02-22 22:35:12 +0900

    feat: 테마 기능 추가

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -1,9 +1,9 @@
 import { Link, Outlet } from "react-router";
-import { SunIcon } from "lucide-react";
 
 import logo from "@/assets/logo.png";
 
 import ProfileButton from "@/components/layout/header/profile-button";
+import ThemeButton from "@/components/layout/header/theme-button";
 
 export default function GlobalLayout() {
   return (
@@ -11,17 +11,11 @@ export default function GlobalLayout() {
       <header className="h-15 border-b">
         <div className="m-auto flex h-full w-full max-w-175 justify-between px-4">
           <Link to={"/"} className="flex items-center gap-2">
-            <img
-              className="h-5"
-              src={logo}
-              alt="한입 로그의 로고, 메시지 말풍선을 형상화한 모양"
-            />
+            <img className="h-5" src={logo} alt="한입 로그의 로고, 메시지 말풍선을 형상화한 모양" />
             <div className="font-bold">한입 로그</div>
           </Link>
           <div className="flex items-center gap-5">
-            <div className="hover:bg-muted cursor-pointer rounded-full p-2">
-              <SunIcon />
-            </div>
+            <ThemeButton />
             <ProfileButton />
           </div>
         </div>
@@ -29,9 +23,7 @@ export default function GlobalLayout() {
       <main className="m-auto w-full max-w-175 flex-1 border-x px-4 py-6">
         <Outlet />
       </main>
-      <footer className="text-muted-foreground border-t py-10 text-center">
-        @marrydaddy
-      </footer>
+      <footer className="text-muted-foreground border-t py-10 text-center">@marrydaddy</footer>
     </div>
   );
 }
